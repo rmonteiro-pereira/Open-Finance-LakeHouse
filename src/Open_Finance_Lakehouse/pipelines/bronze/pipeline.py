c2b00d414d145004829ba1cb04e41985e7b183df
rmonteiro-pereira <rmonteiropereira1@gmail.com>
2025-04-26 23:10:33 -0300

    Migrating to kedro ( lets not rewrite kedro but worse kkk)

---
+++ src/Open_Finance_Lakehouse/pipelines/bronze/pipeline.py
@@ -0,0 +1,36 @@
+"""
+This is a boilerplate pipeline 'bronze'
+generated using Kedro 0.19.12
+"""
+
+from kedro.pipeline import Pipeline, node
+from .nodes import fetch_bacen_series, fetch_cvm_fundos, save_as_delta
+from pyspark.sql import SparkSession
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return Pipeline([
+        node(
+            func=fetch_bacen_series,
+            inputs=dict(series_id="params:bacen_selic_id"),
+            outputs="bacen_selic_df",
+            name="fetch_bacen_selic_node"
+        ),
+        node(
+            func=fetch_cvm_fundos,
+            inputs=dict(spark=SparkSession.builder.getOrCreate(), year="params:cvm_year", month="params:cvm_month"),
+            outputs="cvm_fundos_df",
+            name="fetch_cvm_fundos_node"
+        ),
+        node(
+            func=save_as_delta,
+            inputs=dict(df="bacen_selic_df", output_path="params:bronze_bacen_path"),
+            outputs=None,
+            name="save_bacen_delta_node"
+        ),
+        node(
+            func=save_as_delta,
+            inputs=dict(df="cvm_fundos_df", output_path="params:bronze_cvm_path"),
+            outputs=None,
+            name="save_cvm_delta_node"
+        ),
+    ])
