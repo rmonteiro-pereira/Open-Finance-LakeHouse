69fbc5f4dbd00001c6f6d5802d1b7835c0d32c38
Rodrigo Monteiro Pereira <rmonteiropereira1@gmail.com>
2025-04-27 02:51:56 -0300

    Migrating to kedro v2

--- src/Open_Finance_Lakehouse/pipelines/reporting/__init__.py
+++ src/Open_Finance_Lakehouse/pipelines/reporting/__init__.py
@@ -1,7 +1,3 @@
 """Complete reporting pipeline for the spaceflights tutorial"""
 
-from .pipeline import create_pipeline
-
-__all__ = ["create_pipeline"]
-
-__version__ = "0.1"
+from .pipeline import create_pipeline # NOQA
