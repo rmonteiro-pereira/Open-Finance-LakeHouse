c982d54ec747901ade91e9a61c1aa9f4e8948e62
Rodrigo Monteiro Pereira <rodrigomonteiropereira1@gmail.com>
2025-05-02 13:37:25 -0300

    pipeline cvm inf di fundo funcionando

--- src/Open_Finance_Lakehouse/settings.py
+++ src/Open_Finance_Lakehouse/settings.py
@@ -3,8 +3,9 @@ from the Kedro defaults. For further information, including these default values
 https://docs.kedro.org/en/stable/kedro_project_setup/settings.html."""
 
 # Instantiated project hooks.
-from open_finance_lakehouse.hooks import SparkHooks  # noqa: E402
 from kedro_mlflow.framework.hooks import MlflowHook
+
+from open_finance_lakehouse.hooks import SparkHooks  # noqa: E402
 
 # Hooks are executed in a Last-In-First-Out (LIFO) order.
 HOOKS = (SparkHooks(),)
