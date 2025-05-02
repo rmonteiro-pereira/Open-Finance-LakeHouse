6d6a07b6bd1ed3f222e47e5a04d8bcdf6f673c8b
Rodrigo Monteiro Pereira <rodrigomonteiropereira1@gmail.com>
2025-05-02 12:23:40 -0300

    silver working on notebook

--- src/Open_Finance_Lakehouse/utils/spark_session.py
+++ src/Open_Finance_Lakehouse/utils/spark_session.py
@@ -1,6 +1,12 @@
+import os
+import sys
 from functools import lru_cache
 
+from dotenv import load_dotenv
 from pyspark.sql import SparkSession
+
+load_dotenv()
+
 
 
 @lru_cache(maxsize=1)
@@ -9,15 +15,24 @@ def get_spark_session(app_name: str = "OpenFinanceLakehouse") -> SparkSession:
     Retorna uma SparkSession singleton usando cache interno.
     Não usa variáveis globais.
     """
+    os.environ["PYSPARK_PYTHON"] = sys.executable
+    os.environ["PYSPARK_DRIVER_PYTHON"] = sys.executable
     spark = (
         SparkSession.builder
         .appName(app_name)
-        # .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
-        # .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
-        # Configurações adicionais opcionais (ótimas para ambiente local)
-        # .config("spark.sql.shuffle.partitions", "8")
-        .config("spark.executor.memory", "8g")
-        .config("spark.driver.memory", "8g")
+        .config("spark.driver.host", "localhost")
+        .config("spark.driver.bindAddress", "localhost")
+        .config("spark.jars.packages", "io.delta:delta-spark_2.12:3.1.0,org.apache.hadoop:hadoop-aws:3.3.4")
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+        .config("spark.sql.shuffle.partitions", "8")
+        .config("spark.hadoop.fs.s3a.endpoint", "http://localhost:9000")
+        .config("spark.hadoop.fs.s3a.access.key", os.getenv("MINIO_USER"))
+        .config("spark.hadoop.fs.s3a.secret.key", os.getenv("MINIO_PASSWORD"))
+        .config("spark.hadoop.fs.s3a.path.style.access", "true")
+        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+        .config("spark.executor.memory", "16g")
+        .config("spark.driver.memory", "16g")
         .getOrCreate()
     )
     return spark
